c9fe68c5d7d1bcbd166862bab7159585262ab7d6
cdup <cdup@MSI>
2020-11-20 14:35:41 +1300

    Fixed error on Profile Page

--- Hangman/Hangman/Pages/ProfilePage.xaml.cs
+++ Hangman/Hangman/Pages/ProfilePage.xaml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Hangman.Pages
+namespace Hangman
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
